0786d9da2dfce1221a028e1f064b1e040f23bc09
jawide <596929059@qq.com>
2022-04-11 11:07:45 +0800

    设置publicPath可以通过环境变量BASE_URL配置，默认值为根目录

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: []
-    }
-  },
+    transpileDependencies: true,
+    pluginOptions: {
+        'style-resources-loader': {
+            preProcessor: 'scss',
+            patterns: []
+        },
+    },
+    publicPath: process.env.VUE_APP_PUBLIC_PATH || '/',
 })
